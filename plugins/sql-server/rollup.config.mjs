dca2e6b272482f285ff4ae2c7c86b82110cd0b9c
Kieran Northime <kieran@blackeyetech.com>
2023-02-21 08:10:18 +0000

    added plugin for sql-server

---
+++ plugins/sql-server/rollup.config.mjs
@@ -0,0 +1,61 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import replace from "@rollup/plugin-replace";
+import terser from "@rollup/plugin-terser";
+import sourcemaps from "rollup-plugin-sourcemaps";
+
+import { readFileSync } from "fs";
+
+// Consts here
+
+// Load the package.json so we can get the version
+const pkg = JSON.parse(
+  readFileSync(new URL("./package.json", import.meta.url), "utf8"),
+);
+
+// We need to know if we are building for prod or dev
+const NODE_ENV =
+  process.env.NODE_ENV === undefined ? "development" : process.env.NODE_ENV;
+
+// Setup the plugins here
+let plugins = [
+  replace({
+    preventAssignment: true,
+    values: { PLUGIN_VERSION: pkg.version },
+  }),
+  commonjs(),
+  resolve({ preferBuiltins: true }),
+  json(),
+];
+
+if (NODE_ENV === "development") {
+  plugins.push(sourcemaps);
+} else {
+  plugins.push(terser());
+}
+
+export default [
+  {
+    input: "dist/plugin.js",
+    output: {
+      sourcemap: NODE_ENV === "development",
+      file: "dist/plugin.mjs",
+      format: "es",
+    },
+    external: ["app-sh"],
+
+    plugins,
+  },
+  {
+    input: "dist/plugin.js",
+    output: {
+      sourcemap: NODE_ENV === "development",
+      file: "dist/plugin.cjs",
+      format: "cjs",
+    },
+    external: ["app-sh"],
+
+    plugins,
+  },
+];
